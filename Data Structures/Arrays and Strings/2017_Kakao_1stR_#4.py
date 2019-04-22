ff2c0df6693cf418f3b77342fa581f58ab73a508
suyeon0506 <leesuyeon0506@gmail.com>
2019-04-22 21:27:14 +0800

    190422 kakao coding test 2017 1st round #4

---
+++ Data Structures/Arrays and Strings/2017_Kakao_1stR_#4.py
@@ -0,0 +1,55 @@
+def solution(n, t, m, timetable):
+    bus = n
+    time = "09:00"
+    timetable.sort()
+    
+    while bus > 0:
+        if bus == 1: #last bus is coming
+            if len(timetable) < m:
+                return time
+            else:
+                lastTime = timetable[m-1]
+                break
+                
+        for i in range(m):
+            if timetable[0] <= time:
+                del timetable[0]
+            else:
+                break
+                
+        time = nextbus(time, t)
+        bus -= 1  
+        
+    answer = subtime(lastTime)
+    if answer > time:
+        answer = time
+    
+    return answer
+
+def nextbus(time, t): #get next bus arriving time as string format
+    hour = int(time[:2])
+    minute = int(time[3:])
+    minute += t
+    if minute >= 60:
+        hour += 1
+        minute -= 60
+    if hour < 10:
+        hour = '0' + str(hour)
+    if minute < 10:
+        minute = '0' + str(minute)
+        
+    return str(hour) + ":" + str(minute)
+
+def subtime(time): #subtract one minute of time and return as string format
+    hour = int(time[:2])
+    minute = int(time[3:])
+    if minute == 0:
+        minute = 60
+        hour -= 1
+    minute -= 1
+    if hour < 10:
+        hour = '0' + str(hour)
+    if minute < 10:
+        minute = '0' + str(minute)
+        
+    return str(hour) + ":" + str(minute)
